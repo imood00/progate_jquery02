387619033f409c752edddfe4c776c40839587e4b
imood <69391851+imood00@users.noreply.github.com>
2020-09-29 12:06:02 +0100

    Add files via upload

---
+++ jquery_study_2/page3/script.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('#login-show').click(function() {
+    $('#login-modal').fadeIn();
+  });
+  
+  // 「.signup-show」のclickイベントを作成してください
+  $(".signup-show").click(function(){
+    $("#signup-modal").fadeIn();
+    
+    
+  });
+  
+});
